cd2f004794804dca6a7a681879d62a74b3924061
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-08-06 18:30:03 +0100

    Gallery section almost done

---
+++ src/js/lightGallery.js
@@ -0,0 +1,6 @@
+lightGallery(document.getElementById('lightgallery'), {
+	selector: '.gallery__box-item',
+	plugins: [lgZoom, lgThumbnail],
+	speed: 500,
+	// ... other settings
+});
--- src/js/script.js
+++ src/js/script.js
@@ -3,16 +3,13 @@
 const burgerBtn = document.querySelector('.top-bar__down-hamburger');
 const mobileNav = document.querySelector('.top-bar__down-nav--mobile');
 const mainNav = document.querySelector('.top-bar__down');
+const mediaQuery = window.matchMedia('(min-width: 768px)');
 const DISTANCE_FROM_TOP = 300;
 let flag = false;
 
 const handleNav = function () {
 	mobileNav.classList.toggle('top-bar__down-nav--mobile-active');
 };
-
-window.addEventListener('scroll', function () {
-	flag = true;
-});
 
 const reduceScrollThrottling = function () {
 	if (flag) {
@@ -30,6 +27,29 @@ const reduceScrollThrottling = function () {
 	}
 };
 
+const swapImageSrcAndDataSet = function () {
+	const imagesArr = document.querySelectorAll('.gallery__box-item img');
+
+	imagesArr.forEach((img) => {
+		const img_src = img.src;
+		const parentDataSet = img.parentElement.dataset.src;
+
+		if (mediaQuery.matches) {
+			img.src = img_src.replace('sm', 'lg');
+			img.parentElement.dataset.src = parentDataSet.replace('sm', 'lg');
+		} else {
+			img.src = img_src.replace('lg', 'sm');
+			img.parentElement.dataset.src = parentDataSet.replace('lg', 'sm');
+		}
+	});
+};
+
 setInterval(reduceScrollThrottling, 300);
 
+window.addEventListener('load', swapImageSrcAndDataSet);
+mediaQuery.addEventListener('change', swapImageSrcAndDataSet);
+
+window.addEventListener('scroll', function () {
+	flag = true;
+});
 burgerBtn.addEventListener('click', handleNav);
